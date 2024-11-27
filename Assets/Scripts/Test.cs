43e0b8a135977b6b988f76e70731fc06974b150f
nedilya59630 <148558049+nedilya59630@users.noreply.github.com>
2024-11-27 14:58:45 +0100

    Reapply "Merge branch 'master' of https://github.com/nedilya59630/TestRepoSAE-OCE"
    This reverts commit c0525c042725c8dbe8e77d6d668be1540405dda3.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private new Transform transform;
+
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NOPE
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        Quaternion currentRotation = transform.rotation;
+        Quaternion deltaRotationX = Quaternion.Euler(10 * Time.deltaTime, 0, 0);
+        Quaternion deltaRotationY = Quaternion.Euler(0, 20 * Time.deltaTime, 0);
+        Quaternion deltaRotationZ = Quaternion.Euler(0, 0, 25 * Time.deltaTime);
+        currentRotation *= deltaRotationX;
+        currentRotation *= deltaRotationY;
+        currentRotation *= deltaRotationZ;
+
+        // Setze die neue Rotation
+        transform.rotation = currentRotation;
+    }
+}
