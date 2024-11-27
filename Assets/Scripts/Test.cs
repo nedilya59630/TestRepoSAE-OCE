012fb46b1f450ff2bb45daf459019c952641e2a2
SonNaver <m.giessler@nefkom.net>
2024-11-27 14:31:10 +0100

    A Sphere with some cubes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    private new Transform transform;
+
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,16 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+ 
+        Quaternion currentRotation = transform.rotation;
+        Quaternion deltaRotationX = Quaternion.Euler(10 * Time.deltaTime, 0, 0);
+        Quaternion deltaRotationY = Quaternion.Euler(0, 20 * Time.deltaTime, 0);
+        Quaternion deltaRotationZ = Quaternion.Euler(0, 0, 25 * Time.deltaTime);
+        currentRotation *= deltaRotationX;
+        currentRotation *= deltaRotationY;
+        currentRotation *= deltaRotationZ;
+
+        // Setze die neue Rotation
+        transform.rotation = currentRotation;
     }
 }
